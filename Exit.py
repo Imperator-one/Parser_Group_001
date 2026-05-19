14426c2e17986e6cf27f9393992653cfa11cc79e
Imperator | I always comeback <abdlazimovtima@gmail.com>
2026-05-19 14:51:33 +0300

    Update Exit.py
    Доработал систему json, работает или нет проверим в академии, но в вероятнее работает. Убрал ненужное, добавил функцию

--- Exit.py
+++ Exit.py
@@ -1,16 +1,4 @@
 import json
-user_profile = {
-    "user_id": 123,
-    "username": "coder2025",
-    "settings": {
-        "theme": "dark",
-        "notifications": True
-    }
-}
-
-with open("profile.json", "w", encoding="utf-8") as f:
-    json.dump(user_profile, f, indent=4, ensure_ascii=False)
-
-# txt = {1:"Я лучший, среди худших"}
-# with open("profile.json", "w", encoding="utf-8") as file:
-#     json.dump(txt, file)
+def exit(txt):
+    with open("per.json", "w", encoding="utf-8") as file:
+    json.dump(txt, file)
